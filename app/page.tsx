12ce69ee877419e4f99ea543e8f498adf0345bac
tBrause <brause.torsten@gmail.com>
2023-08-18 19:05:32 +0200

    edit & delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-	return <h1>Next, React &amp; TypeScript</h1>;
+	return (
+		<>
+			<div>
+				<picture
+					style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}
+				>
+					<img style={{ width: '20vw' }} src="/next.svg" alt="Next" />
+				</picture>
+				<picture
+					style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}
+				>
+					<img style={{ width: '20vw' }} src="/react.svg" alt="React" />
+				</picture>
+				<picture
+					style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}
+				>
+					<img
+						style={{ width: '20vw' }}
+						src="/typescript.svg"
+						alt="TypeScript"
+					/>
+				</picture>
+			</div>
+		</>
+	);
 }
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function MainNav() {
 	return (
-		<nav>
+		<nav style={{ display: 'none' }}>
 			<ul>
 				<li>
 					<Link href="/">Home</Link>
